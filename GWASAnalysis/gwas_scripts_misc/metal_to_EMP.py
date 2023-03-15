da3de5a6175470d425421192d10b1a52b0e26cdd
DashaZhernakova <dashazhernakova@gmail.com>
2023-03-15 11:49:37 +0100

    bugfix

--- GWASAnalysis/gwas_scripts_misc/metal_to_EMP.py
+++ GWASAnalysis/gwas_scripts_misc/metal_to_EMP.py
@@ -66,7 +66,7 @@ for l in f:
             zscores += ";-1"
     
     # Skip results tested in less than 2 cohorts    
-    if len(";".split(datasets)) < 2:
+    iif len(datasets.split(";")) < 2:
         continue
     
     zscores = zscores.replace(";","", 1)
